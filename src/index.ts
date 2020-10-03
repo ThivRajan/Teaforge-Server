405459ec9520f8afb92ffeafbd122c798f2d0928
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-03 13:15:16 -0400

    socket connections

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import socket from 'socket.io';
 
+import { Games, Player } from './types';
+
 const app = express();
 
 const PORT = 3001;
@@ -8,11 +10,21 @@ const server = app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
 
-const io = socket(server);
-io.on('connection', () => {
-	console.log('connection made');
-});
-
 app.get('/', (_req, res) => {
 	res.send('server is running');
 });
+
+const io = socket(server);
+let players: Array<Player> = [];
+
+io.of(`/${Games.Resistance}`).on('connection', (socket) => {
+	socket.on('create', (name: string) => {
+		players = players.concat({ id: socket.id, name });
+		socket.emit('roomKey', 'key');
+		socket.join('room', () => {
+			const playerNames = players.map(p => p.name);
+			io.to('room').emit('players', playerNames);
+		});
+	});
+});
+
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Player {
+	id: string;
+	name: string;
+}
+
+export interface Error {
+	error: string;
+}
+
+export enum Games {
+	Resistance = 'resistance'
+}
