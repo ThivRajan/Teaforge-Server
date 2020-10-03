1b666cfd2e78ac6e37c295010d20703b402fbbda
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-03 13:15:43 -0400

    build update

--- build/index.js
+++ build/index.js
@@ -5,15 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const socket_io_1 = __importDefault(require("socket.io"));
+const types_1 = require("./types");
 const app = express_1.default();
 const PORT = 3001;
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-const io = socket_io_1.default(server);
-io.on('connection', () => {
-    console.log('connection made');
-});
 app.get('/', (_req, res) => {
     res.send('server is running');
+});
+const io = socket_io_1.default(server);
+let players = [];
+io.of(`/${types_1.Games.Resistance}`).on('connection', (socket) => {
+    socket.on('create', (name) => {
+        players = players.concat({ id: socket.id, name });
+        socket.emit('roomKey', 'key');
+        socket.join('room', () => {
+            const playerNames = players.map(p => p.name);
+            io.to('room').emit('players', playerNames);
+        });
+    });
 });
