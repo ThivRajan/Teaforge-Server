c99cd87b442be2b0d38ca5a3a73bf2b10c9d74ad
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-04 17:29:49 -0400

    making rooms generic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import socket from 'socket.io';
 
-import { Games } from './types';
+// import { Games } from './types';
 import { generateKey } from './utils';
 
 const app = express();
@@ -22,42 +22,49 @@ interface Players {
 }
 const players: Players = {};
 
+//TODO: deal with type error when client leaves room (prob has to do with socket.onclose)
+// i think this error has to do with the server disconnecting & reconnecting
+
 //TODO: delete room if 0 players left in room
 //TODO: remove player or delete room on disconnect if host  
 //TODO: make max capacity for room
-
-//TODO: Error handling
-//TODO: check if name is unique in room (part of error handling)
-io.of(`/${Games.Resistance}`).on('connection', (socket) => {
+io.on('connection', (socket) => {
 	socket.on('create', (name: string) => {
-		const key = generateKey();
-		if (io.nsps[`/${Games.Resistance}`].adapter.rooms[key]) {
-			console.log('ERROR');
-			// TODO: throw error or something
+		let key = generateKey();
+		/* Generate unique key */
+		while (io.sockets.adapter.rooms[key]) {
+			key = generateKey();
 		}
-		players[socket.id] = { name, key };
+
+		players[socket.id] = { name: name.trim(), key };
 		socket.join(`${key}`);
 		socket.emit('roomKey', key);
+		socket.emit('players', [name]);
 	});
 
 	socket.on('join', (name: string, key: string) => {
 		players[socket.id] = { name, key };
-		if (io.nsps[`/${Games.Resistance}`].adapter.rooms[key]) {
-			socket.emit('validKey');
-			socket.join(`${key}`, () => {
-				const roomSockets = io.nsps[`/${Games.Resistance}`].adapter.rooms[key].sockets;
-				const playerIds = Object.keys(roomSockets);
-				const playerNames = playerIds.map((id): string => players[id].name);
+		if (io.sockets.adapter.rooms[key]) {
+			const roomSockets = io.sockets.adapter.rooms[key].sockets;
+			const playerIds = Object.keys(roomSockets);
+			const playerNames = playerIds.map((id): string => players[id].name);
 
-				io.of(`/${Games.Resistance}`).in(`${key}`).emit('players', playerNames);
-			});
+			name = name.trim();
+			if (!playerNames.find(n => n.toLowerCase() === name.toLowerCase())) {
+				socket.emit('valid');
+				socket.join(`${key}`);
+				playerNames.push(name);
+				io.of('/').in(`${key}`).emit('players', playerNames);
+			} else {
+				socket.emit('invalid', 'Name is already taken');
+			}
 		} else {
-			socket.emit('invalidKey');
+			socket.emit('invalid', 'Key is invalid');
 		}
 	});
 
 	socket.on('getPlayers', (key: string) => {
-		const roomSockets = io.nsps[`/${Games.Resistance}`].adapter.rooms[key].sockets;
+		const roomSockets = io.sockets.adapter.rooms[key].sockets;
 		const playerIds = Object.keys(roomSockets);
 		const playerNames = playerIds.map((id): string => players[id].name);
 
@@ -65,15 +72,16 @@ io.of(`/${Games.Resistance}`).on('connection', (socket) => {
 	});
 
 	socket.on('disconnect', () => {
-		console.log('disconnect');
 		const key = players[socket.id].key;
 		delete players[socket.id];
 
-		const roomSockets = io.nsps[`/${Games.Resistance}`].adapter.rooms[key].sockets;
+		const roomSockets = io.sockets.adapter.rooms[key].sockets;
 		const playerIds = Object.keys(roomSockets);
 		const playerNames = playerIds.map((id): string => players[id].name);
 
-		io.of(`/${Games.Resistance}`).in(`${key}`).emit('players', playerNames);
+		io.of('/').in(`${key}`).emit('players', playerNames);
 	});
 });
+
+
 
